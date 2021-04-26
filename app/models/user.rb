640ba57f3287c1136f5f06cb72da2b6174282095
00ryohey00 <ar5vvv31@icloud.com>
2021-04-26 22:04:27 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  VALID_PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/
-  validates :password, format: { with: VALID_PASSWORD_REGEX },length: { minimum: 2}
-  validates :lastname , :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :lastname_kana , :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday , presence: true
-  validates :nickname , presence: true
+  VALID_PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 2 }
+  validates :lastname, :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :lastname_kana, :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
+  validates :nickname, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
