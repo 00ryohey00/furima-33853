64f10d4207e47c221277abf30c96051ea7497e8b
00ryohey00 <ar5vvv31@icloud.com>
2021-04-27 22:45:35 +0900

    ログインキーの許可

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   VALID_PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 2 }
+  validates :password, :password_confirmation, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 2 }
   validates :lastname, :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :lastname_kana, :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
   validates :nickname, presence: true
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:email, :password]
 end
