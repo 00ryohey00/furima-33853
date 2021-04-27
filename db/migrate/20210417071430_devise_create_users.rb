13fb346daaa16f2c4ce33881c26ce26e33b1aea7
00ryohey00 <ar5vvv31@icloud.com>
2021-04-27 22:47:17 +0900

    カラム名の修正

--- db/migrate/20210417071430_devise_create_users.rb
+++ db/migrate/20210417071430_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nickname,  null: false
       t.string :email,              null: false, default: ""
+      # t.string :encrypted_password, null: false, default: ""
       t.string :password, null: false, default: ""
       t.string :password_confirmation, null: false, default: ""
       t.string :lastname,  null: false
