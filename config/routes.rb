3412b4a1f2d62d495ca1dfa560022f714436d2ea
00ryohey00 <ar5vvv31@icloud.com>
2021-04-30 22:18:33 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
+  # get    '/login',   to: 'sessions#new'
+  # post   '/login',   to: 'sessions#create'
+  # delete '/logout',  to: 'sessions#destroy'
   resources :items, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
