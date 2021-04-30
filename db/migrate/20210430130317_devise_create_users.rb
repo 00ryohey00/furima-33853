5672d416099dda3e3431190b5361cc81fa59f3fb
00ryohey00 <ar5vvv31@icloud.com>
2021-04-30 22:19:04 +0900

    マイグレーションファイルの編集

--- db/migrate/20210430130317_devise_create_users.rb
+++ db/migrate/20210430130317_devise_create_users.rb
@@ -4,11 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,  null: false
       t.string :email,              null: false, default: ""
-      # t.string :encrypted_password, null: false, default: ""
-      t.string :password, null: false, default: ""
-      t.string :password_confirmation, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,  null: false
       t.string :lastname,  null: false
       t.string :firstname,  null: false
       t.string :lastname_kana,  null: false
