68efedf448e2afe97092545e135e8eca709c4da8
00ryohey00 <ar5vvv31@icloud.com>
2021-04-30 22:17:32 +0900

    バリデーション更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  VALID_PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/.freeze
-  validates :password, :password_confirmation, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 2 }
-  validates :lastname, :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :lastname_kana, :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
-  validates :nickname, presence: true
-
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, authentication_keys: [:email, :password]
+  :recoverable, :rememberable, :validatable
+  
+         VALID_PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/.freeze
+         validates :password, :password_confirmation, format: { with: VALID_PASSWORD_REGEX }, length: { minimum: 2 }
+         validates :lastname, :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :lastname_kana, :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :birthday, presence: true
+         validates :nickname, presence: true
+       
+
 end
