964748bb3831429897ffa34b7122f15328ad0f67
00ryohey00 <ar5vvv31@icloud.com>
2021-05-08 01:20:55 +0900

    商品出品ページのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
-class Item < ApplicationRecord
-end
+  class Item < ApplicationRecord
+    has_one_attached :image
+
+    validates :price, format: { with: /\A[０-９]+\z/}
+    with_options presence: true do
+      validates :image
+      validates :title
+      validates :explain
+      validates :category_id
+      validates :status_id
+      validates :financial_id
+      validates :prefecture_id
+      validates :day_id
+      validates :price
+    end
+    validates :category_id, :day_id, :financial_id, :prefecture_id, :status_id, numericality: { other_than: 1 } 
+ 
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category, :day, :financial, :prefecture, :status
+
+  end
