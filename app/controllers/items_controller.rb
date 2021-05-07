928c4bebff6791f3e342bf64a84185280e3cfc3c
00ryohey00 <ar5vvv31@icloud.com>
2021-05-07 09:51:18 +0900

    createアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,13 @@ class ItemsController < ApplicationController
   def new 
     @item = Item.new
   end
+
+  def create
+    @item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :title, :explain, :category_id, :status_id, :financial_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
+  end
 end
