3e5e5c976fab4734126b9d178e5d62f0d2d48c3d
00ryohey00 <ar5vvv31@icloud.com>
2021-05-08 13:23:38 +0900

    販売手数料、販売利益のJS実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function(){
+
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+   const inputValue = priceInput.value;
+   console.log(inputValue);
+
+   const addTaxDom = document.getElementById("add-tax-price");
+   Salescommission = Math.floor(inputValue*0.1)
+    addTaxDom.innerHTML= Salescommission
+
+   const salesProfit = document.getElementById("profit");
+   profit = (inputValue - Salescommission)
+    salesProfit.innerHTML= profit
+})
+
+})
