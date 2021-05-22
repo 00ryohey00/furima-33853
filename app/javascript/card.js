2d1750f0437e59dd4752f3cbf88cfdc21655f431
00ryohey00 <ar5vvv31@icloud.com>
2021-05-22 20:25:18 +0900

    jsファイルの確認

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -12,7 +12,7 @@ const pay = () => {
       exp_month: formData.get("buyer_address[card_exp_month]"),
       exp_year: `20${formData.get("buyer_address[card_exp_year]")}`,
     };
-
+    console.log(card)
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
